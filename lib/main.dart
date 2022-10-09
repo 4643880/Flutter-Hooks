4b76f79acb1f818536a3c41af09111e159d9b0c6
Aizaz Haider <aizazhaider720@gmail.com>
2022-10-09 23:37:31 +0500

    useAppLifeCycleState Hook Ends Here with ATM Card Example

--- lib/main.dart
+++ lib/main.dart
@@ -23,54 +23,25 @@ class MyApp extends StatelessWidget {
   }
 }
 
-const imageUrl =
-    "https://cdn.pixabay.com/photo/2022/09/13/17/02/leaves-7452420_960_720.jpg";
-
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    double rotation = 0;
-
-    late final StreamController<double> controller;
-    controller = useStreamController<double>(
-      onListen: () {
-        controller.sink.add(0.0);
-      },
-    );
+    final state = useAppLifecycleState();
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter Hooks"),
       ),
-      body: StreamBuilder(
-        stream: controller.stream,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            rotation = snapshot.data ?? 0;
-            return GestureDetector(
-              onTap: () {
-                controller.sink.add(rotation + 30);
-              },
-              child: RotationTransition(
-                turns: AlwaysStoppedAnimation(rotation / 360),
-                child: const Center(
-                  child: Padding(
-                    padding: EdgeInsets.all(32.0),
-                    child: Image(
-                      image: NetworkImage(imageUrl),
-                    ),
-                  ),
-                ),
-              ),
-            );
-          }
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Opacity(
+          opacity: (state == AppLifecycleState.resumed) ? 1.0 : 0.0,
+          child: Container(
+            child: Image.asset("assets/1.png"),
+          ),
+        ),
       ),
     );
   }
