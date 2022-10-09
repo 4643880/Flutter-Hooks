5443288c9063bb0b56a815ee472c24966dc63476
Aizaz Haider <aizazhaider720@gmail.com>
2022-10-09 23:07:00 +0500

    useStreamController Hook Ends Here with Image Rotation Example

--- lib/main.dart
+++ lib/main.dart
@@ -23,55 +23,54 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class CountDown extends ValueNotifier<int> {
-  late StreamSubscription sub;
-
-  CountDown({required int from}) : super(from) {
-    sub = Stream.periodic(
-      const Duration(seconds: 1),
-      (int myValue) => from - myValue,
-    )
-        .takeWhile(
-      (element) => element >= 0,
-    )
-        .listen((event) {
-      value = event;
-      notifyListeners();
-    });
-  }
-
-  @override
-  void dispose() {
-    sub.cancel();
-    super.dispose();
-  }
-}
+const imageUrl =
+    "https://cdn.pixabay.com/photo/2022/09/13/17/02/leaves-7452420_960_720.jpg";
 
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    CountDown obj = CountDown(from: 20);
-    // I don't want to create the instance again and again using memoized
-    final objAgain = useMemoized(() => obj);
-    final myListenable = useListenable(objAgain);
-    print("Hello");
+    double rotation = 0;
+
+    late final StreamController<double> controller;
+    controller = useStreamController<double>(
+      onListen: () {
+        controller.sink.add(0.0);
+      },
+    );
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter Hooks"),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(myListenable.value.toString()),
-            ],
-          ),
-        ),
+      body: StreamBuilder(
+        stream: controller.stream,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            rotation = snapshot.data ?? 0;
+            return GestureDetector(
+              onTap: () {
+                controller.sink.add(rotation + 30);
+              },
+              child: RotationTransition(
+                turns: AlwaysStoppedAnimation(rotation / 360),
+                child: const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(32.0),
+                    child: Image(
+                      image: NetworkImage(imageUrl),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }
+        },
       ),
     );
   }
