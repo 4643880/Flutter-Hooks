74b7fc9cc6b6025a6972a0963c1515bfce8afec7
Aizaz Haider <aizazhaider720@gmail.com>
2022-10-09 21:45:39 +0500

    useListenable Hook Ends Here

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:intl/intl.dart'; // for date format
-import 'package:intl/date_symbol_data_local.dart'; // for other locales
 
 void main() {
   runApp(const MyApp());
 }
-
-extension CompactMap<T> on Iterable<T?> {}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -25,22 +23,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class CountDown extends ValueNotifier<int> {
+  late StreamSubscription sub;
+
+  CountDown({required int from}) : super(from) {
+    sub = Stream.periodic(
+      const Duration(seconds: 1),
+      (int myValue) => from - myValue,
+    )
+        .takeWhile(
+      (element) => element >= 0,
+    )
+        .listen((event) {
+      value = event;
+      notifyListeners();
+    });
+  }
+
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+}
+
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final count = useState(0);
-    count.value++;
-
-    Future<String> fetchData() async {
-      await Future.delayed(const Duration(seconds: 3));
-      return "Hello Aizaz ${count.value}";
-    }
-
-    // We use it for Caching the data that came from future first time will remain same
-    final myMemoizedHook = useMemoized(fetchData);
-    final myFutureHook = useFuture(myMemoizedHook);
+    CountDown obj = CountDown(from: 20);
+    // I don't want to create the instance again and again using memoized
+    final objAgain = useMemoized(() => obj);
+    final myListenable = useListenable(objAgain);
+    print("Hello");
 
     return Scaffold(
       appBar: AppBar(
@@ -52,17 +68,7 @@ class HomePage extends HookWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Padding(
-                padding: EdgeInsets.all(32.0),
-              ),
-              (myFutureHook.hasData)
-                  ? Text(
-                      myFutureHook.data.toString(),
-                      style: Theme.of(context).textTheme.headline5,
-                    )
-                  : const Center(
-                      child: CircularProgressIndicator(),
-                    )
+              Text(myListenable.value.toString()),
             ],
           ),
         ),
